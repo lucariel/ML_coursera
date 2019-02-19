150198a5da0cee3efacd27d22bfa0d8f5a05cea0
lucariel <lucariel92@gmail.com>
2019-02-19 09:05:03 -0300

    predictions ex2

--- machine-learning-ex2/R version/ex2.R
+++ machine-learning-ex2/R version/ex2.R
@@ -7,9 +7,23 @@ e<-exp(1)
 data <- read.delim("ex2data1.txt", header = F, sep = ",")
 colnames(data)<-c("exam1", "exam2", "accepted")
 data%>%ggplot(aes(x = exam1, y = exam2, color = accepted))+geom_point()
+
+
+
+
+
+
+
+
 X<-data[,1:2]
 X <- cbind(X, as.vector(matrix(1,1, length(data$exam1))))
 X <- as.matrix(cbind(X[,3],X[,1],X[,2]))
+##Normalize features
+
+X[,2] <- (X[,2]-mean(X[,2]))/sd(X[,2])
+X[,3] <- (X[,3]-mean(X[,3]))/sd(X[,3])
+
+
 
 y<-data[,3]
 theta<- as.vector(c(0,0,0))
@@ -35,17 +49,47 @@ costFunction_lg(theta, X, y)
   #αXT(g(Xθ)−y)
 
 gradientDescent<-function(X, y, theta, alpha, num_iter){
-  control<- costFunction_lg(theta, X, y)
+  J_hist <- rep(0, num_iter)
   m <- length(y)
   for(i in 1:num_iter){
     h <- sigmoid(X%*%theta)
     theta<- theta-(alpha/m)*t(X)%*%(h-y)
-    control<-rbind(control,costFunction_lg(theta, X, y))
+    J_hist[num_iter]<-costFunction_lg(theta, X, y)
   }
   return(theta)
 
 }
 
-gradientDescent(X,y,theta, 0.005, 10)
 
+#Learned: without the advance optimization algorithm, this takes a lot of time in my computer
+alpha <- .1
+i <- 200000
+results <- gradDescent(X, y, theta, 0.001, 200000)
+theta <- results[[1]]
+cost_hist <- results[[2]]
+print(theta)
+plot(1:i, cost_hist)
+
+#############Predicting#########
+
+theta2<-c(-25.161272,0.206233,0.201470) ##From octave's solution, as reference
+
+##De-normalization of X
+
+
+Xn<-data[,1:2]
+Xn <- cbind(X, as.vector(matrix(1,1, length(data$exam1))))
+Xn <- as.matrix(cbind(X[,1],X[,2],X[,3]))
+
+
+prediction<-round(sigmoid(Xn%*%theta))
+1-sum(abs(prediction-y))/100 
+
+
+## Decision boundary at x2 = (1-/theta2)*(theta_0+theta1*x1)
+
+###to do: calculate boundary
+
+data%>%ggplot(aes(x = exam1, y = exam2, color = accepted))+geom_point()+
+  geom_segment(aes(x = 100, y = 0, xend = 0, yend = 100))
 
